96267f88964f8a8fdca71c881e5f7bda6002cd18
pandeyaniket <55311640+pandeyaniket@users.noreply.github.com>
2021-05-03 10:21:01 +0530

    Add files via upload

---
+++ lab exam .py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Aniket Pandey, A71004818009, BCA SEM6")
+print("Python file")
+print("this file is created for lab exam")
+print("Date 3/5/21")
+
+
+# In[ ]:
+
+
+
+
